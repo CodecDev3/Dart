459ed2f6fa10f2e0cd95fc18417bf8f3f55caf8b
Devavrat Patel <113817588+CodecDev3@users.noreply.github.com>
2023-10-14 19:25:17 +0530

    Create 06_conditional_expressions.dart

---
+++ 06_conditional_expressions.dart
@@ -0,0 +1,30 @@
+
+void main() {
+
+	// Conditional Expressions
+
+	// 1.   condition ? exp1 : exp2
+	// If condition is true, evaluates expr1 (and returns its value);
+	// otherwise, evaluates and returns the value of expr2.
+
+	int a = 2;
+	int b = 3;
+
+  // basic condition 
+  a < b ? print("a is lesser") : print("b is lesser");
+
+  // store value of output in smallNumber
+	int smallNumber = a < b ? a : b;
+	print("$smallNumber is smaller");
+
+
+
+	// 2.   exp1 ?? exp2
+	// If expr1 is non-null, returns its value; otherwise, evaluates and
+	// returns the value of expr2.
+
+	String name = null;
+
+	String nameToPrint = name ?? "Guest User";
+	print(nameToPrint);
+}
