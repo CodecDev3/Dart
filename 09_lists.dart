c8c0412505cfa165c22d0e8717365d321e5da988
Devavrat Patel <113817588+CodecDev3@users.noreply.github.com>
2023-10-15 13:03:18 +0530

    Update 09_lists.dart

--- 09_lists.dart
+++ 09_lists.dart
@@ -1,1 +1,18 @@
+void main(){
+  List l1 = ['1',22,'list'];
+
+  // add 1 element at the end
+  l1.add('any text or int');
+
+  // add mutilple elements at the end
+  l1.addAll(['multiple_elements',72,55,'other']);
+
+  // insert element at a position
+  l1.insert(2,'inserted at third position(2)');
+
+  // insert multiple elements after a position
+  l1.insertAll(6,['6th','7th']);
+  
+  print(l1);
+}
 
