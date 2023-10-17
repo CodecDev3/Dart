aeb74cd8b2384fc366e57b03883ff189fe01b8da
Devavrat Patel <113817588+CodecDev3@users.noreply.github.com>
2023-10-17 18:51:31 +0530

    Update 10_Boolean.dart

--- 10_Boolean.dart
+++ 10_Boolean.dart
@@ -6,4 +6,24 @@ void main(){
 
 ***********************
 
- 
+ Operator	Meaning
+is	  True if the object has the specified type
+is!  	False if the object has the specified type
+
+void main() { 
+   int n = 2; 
+   print(n is int); 
+} 
+OUTPUT: true
+
+void main(){
+  double d = 78.55;
+  print(d is! int);
+}
+OUTPUT: false
+
+   String s = "ss";
+  print(s.isEmpty); // returns bool value
+ print(s.isNotEmpty); // returns bool value
+
+}
