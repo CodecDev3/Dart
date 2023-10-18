f67d7251169676380368c644aeea5cdea3034066
Devavrat Patel <113817588+CodecDev3@users.noreply.github.com>
2023-10-18 13:38:39 +0530

    Create 11_map.dart

---
+++ 11_map.dart
@@ -0,0 +1,10 @@
+void main() { 
+   var details = new Map(); 
+   details['Usrname'] = 'admin'; 
+   details['Password'] = 'admin@123'; 
+   print(details); 
+}
+
+*****************************
+
+  
