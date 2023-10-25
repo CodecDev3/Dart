a576e7b5644d7dd4accccdf069e797aa0dc2ceb4
Devavrat Patel <113817588+CodecDev3@users.noreply.github.com>
2023-10-25 16:29:32 +0530

    Create 0_Prac.dart

---
+++ 0_Prac.dart
@@ -0,0 +1,22 @@
+// List and Map
+
+void main(){
+  int a = 10;
+  
+  List list = [];
+  var maplist = {};
+  
+  for(int i = 0; i <= 5; i++){
+    a++;
+    list.insert(i,'${a}');
+  }
+  
+  print(list);
+  
+  for(int i = 0; i < 5; i++){
+    maplist[list[i]] = 'ML_${list[i]}';
+  }
+  print(maplist);
+  
+}
+
